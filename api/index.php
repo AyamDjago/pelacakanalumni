8d0e6ea6b9d0f03c70d146b36431c16a97011e49
AyamDjago <mhilmanhazmi07@gmail.com>
2026-03-14 23:43:34 +0700

    fix vercel

--- api/index.php
+++ api/index.php
@@ -1,2 +1,8 @@
 <?php
+
+putenv('VIEW_COMPILED_PATH=/tmp');
+putenv('CACHE_DRIVER=array');
+putenv('LOG_CHANNEL=stderr');
+putenv('SESSION_DRIVER=cookie');
+
 require __DIR__ . '/../public/index.php';
